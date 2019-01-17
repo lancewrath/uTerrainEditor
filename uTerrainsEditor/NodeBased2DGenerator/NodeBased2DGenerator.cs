637e6aae61b955ab43b6fc91f6c8ebc3ba4b0668
lancewrath <landon.g.ritchie@gmail.com>
2019-01-16 22:55:56 -0800

    Add files via upload

---
+++ uTerrainsEditor/NodeBased2DGenerator/NodeBased2DGenerator.cs
@@ -0,0 +1,29 @@
+﻿using LibNoise;
+using LibNoise.Primitive;
+using UltimateTerrains;
+using Zorlock.uTerrains;
+using UnityEngine;
+
+public class NodeBased2DGenerator : I2DGenerator
+{
+    private readonly Generator generator;
+    private readonly double frequency;
+    private readonly double scale;
+
+    public NodeBased2DGenerator(double frequency, double scale,Generator gen)
+    {
+        this.generator = gen;
+        Debug.Log("reconnecting nodes");
+        this.generator.ReconnectNodes();
+        this.generator.GetFinalOperation();
+        this.frequency = frequency;
+        this.scale = scale;
+
+    }
+
+    public double GetValue(double x, double z)
+    {
+        return this.generator.GetValue(x * frequency, 0, z * frequency) *scale;
+    }
+
+}
---
+++ uTerrainsEditor/NodeBased2DGenerator/NodeBased2DGeneratorSerializable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UltimateTerrains;
+using LibNoise;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using Zorlock.uTerrains;
+
+public class NodeBased2DGeneratorSerializable : Serializable2DGenerator {
+
+    [SerializeField] Generator generator;
+    [SerializeField] float frequency = 1f / 90f;
+    [SerializeField] float scale = 1f;
+    public override void OnEditorGUI(UltimateTerrain uTerrain)
+    {
+#if UNITY_EDITOR
+        frequency = EditorGUILayout.FloatField("Frequency:", frequency);
+        scale = EditorGUILayout.FloatField("Scale:", scale);
+        generator = (Generator)EditorGUILayout.ObjectField(generator,typeof(Generator), false);
+
+#endif
+    }
+
+    public override I2DGenerator CreateModule(UltimateTerrain uTerrain)
+    {
+        return new NodeBased2DGenerator(frequency, scale, generator);
+    }
+
+}
